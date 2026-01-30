1583f322e56971ee08236bff894ce170b3f6d6ce
TOBY0001 <mktoby06@gmail.com>
2026-01-30 14:00:41 +0100

    smol update

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -11,7 +11,10 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml"/>
+          <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=5.0, user-scalable=yes" />
+          <meta name="mobile-web-app-capable" content="yes" />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <link rel="icon" href="/favicon.svg" type="image/svg+xml"/>
         </Head>
         <body>
           <Main />
